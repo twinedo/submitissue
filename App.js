c87143bf872887313f30897e83f9c1cf41ebbb25
Twin Edo <venganzdektor26@gmail.com>
2020-04-27 16:23:32 +0700

    bisa zoom image

--- App.js
+++ App.js
@@ -2,15 +2,14 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import AllComponent from './src/pages/AllComponent';
 import ReasonCategory from './src/pages/ReasonCategory';
-import AddImage from './src/pages/AddImage';
-import ImageCropper from './src/pages/ImageCropper';
 import ImageCropper2 from './src/pages/ImageCropper2';
 import FixSubmit from './src/pages/FixSubmit';
+import FixSubmit2 from './src/pages/FixSubmit2';
 
 const App = () => {
   return (
     <>
-      <FixSubmit />
+      <FixSubmit2 />
       {/*<AllComponent />*/}
       {/* <ReasonCategory /> */}
       {/* <AddImage /> */}
